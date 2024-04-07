f4f0ee81c57ee04a546cafe4797dffb6a9d9529f
Роман Мифтахов <miftahov9548@gmail.com>
2024-04-07 13:33:16 +0300

    refactoring

---
+++ Analysis_Ip/Models/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analysis_Ip.Models
+{
+    internal class Model
+    {
+       public class LogEntry
+        {
+            public string IPAddress { get; set; }
+            public DateTime Time { get; set; }
+        }
+       public class Arguments
+        {
+            public string FileLog { get; set; }
+            public string FileOutput { get; set; }
+            public DateTime TimeStart { get; set; }
+            public DateTime TimeEnd { get; set; }
+            public string AddressStart { get; set; }
+            public int? AddressMask { get; set; }
+        }
+    }
+}
